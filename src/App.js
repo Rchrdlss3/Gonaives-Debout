d88187b5509a4bb59633e2452b65012a3ec05d07
Richard Ulysse <Rchrdlss3@Gmail.com>
2024-04-10 20:02:17 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import { routeMap } from './helper/commonhelper';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        {[...routeMap.values()].map(route => 
+         <Route path = {route.path} element  = {route.element}>{route.name}</Route>
+        )}
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
