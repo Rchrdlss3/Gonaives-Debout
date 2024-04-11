831b60e2c880d0268219796e7d5fa539c172318e
Richard Ulysse <Rchrdlss3@Gmail.com>
2024-04-10 20:20:37 -0400

    switched to HashRouter for navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import {Routes, Route, HashRouter} from 'react-router-dom';
 import { routeMap } from './helper/commonhelper';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
         {[...routeMap.values()].map(route => 
          <Route path = {route.path} element  = {route.element}>{route.name}</Route>
         )}
       </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
